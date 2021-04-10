4621b3d4ff0222cec2cd5e61a15dba16f84fd2ab
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-10 09:47:53 -0500

    Tests to user validations were created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :email, presence: true
+
+  #validates :password, presence: true
+  #validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
+
 end
