947714ffff5e8868c8211edc72af51b2a675e195
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-11 01:29:35 -0500

    Add presence validations and email format to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #validates :password, presence: true
-  #validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
 
 end
