1474b52a9b4d3b76a69f88993fcd1a1c32446a26
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-12 01:31:31 -0500

    Create contacts controller and implement method to import CSV file

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,4 +7,16 @@ class Contact < ApplicationRecord
   validates :credit_card_number, presence: true
   validates :credit_card_franchise, presence: true
 
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      contact_hash = row.to_hash
+      contact = find_or_create_by!(name: contact_hash['name'], telephone: contact_hash['telephone'],
+                                  email: contact_hash['email'], address: contact_hash['address'],
+                                  birthday: contact_hash['birthday'],
+                                  credit_card_number: contact_hash['credit card number'],
+                                  credit_card_franchise: contact_hash['credit card franchise'])
+      contact.update(contact_hash)
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe User, type: :model do
   describe "#validations" do
   let(:user) { User.new() }
 
-    it 'when user missing email' do
+    it 'is missing email' do
       user.email = nil
       expect(user).not_to be_valid
       expect(user.errors[:email]).to include ("can't be blank")
     end
 
-    it 'when user has an invalid email' do
+    it 'is has an invalid email' do
       user.email = "email"
       expect(user).not_to be_valid
     end
 
-    it 'when user missing password' do
+    it 'is missing password' do
       user.password = nil
       expect(user).not_to be_valid
       expect(user.errors[:password]).to include ("can't be blank")
     end
 
-    it 'when user missing password confirmation' do
+    it 'is missing password confirmation' do
       user.password_confirmation = nil
       expect(user).not_to be_valid
     end
 
-    it 'when user has not same password confirmation' do
+    it 'is has not same password confirmation' do
       user.password = "userpassword123*"
       user.password_confirmation="UserPassword123*"
       expect(user).not_to be_valid
     end
 
-    it 'when another user has a duplicated email' do
+    it 'is has a duplicated email' do
       user2 = User.new
       user2.email = "email@mail.com"
       user.email = "email@mail.com"
