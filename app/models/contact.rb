2926963319324d3a489e1f084a48ef3c020f0f0c
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-13 00:13:41 -0500

    Add telephone validation and fix some fields from import method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,9 @@
 class Contact < ApplicationRecord
-  validates :name, presence: true
-  validates :telephone, presence: true
-  validates :email, presence: true
-  validates :address, presence: true
-  validates :birthday, presence: true
-  validates :credit_card_number, presence: true
-  validates :credit_card_franchise, presence: true
+  validates :name, :email, :address, :birthday, :credit_card_number, :credit_card_franchise, presence: true
+  validates :telephone, presence: true,
+            format: { with: /\A(\(\+\d{2}\)[\s]\d{3}[\s]\d{3}[\s]\d{2}[\s]\d{2})|(\(\+\d{2}\)[\s]\d{3}[-]\d{3}[-]\d{2}[-]\d{2})\z/,
+            message: "(+00) 000 000 00 00 and (+00) 000-000-00-00 are the only telephone formats permitted" }
+
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
@@ -13,8 +11,8 @@ class Contact < ApplicationRecord
       contact = find_or_create_by!(name: contact_hash['name'], telephone: contact_hash['telephone'],
                                   email: contact_hash['email'], address: contact_hash['address'],
                                   birthday: contact_hash['birthday'],
-                                  credit_card_number: contact_hash['credit card number'],
-                                  credit_card_franchise: contact_hash['credit card franchise'])
+                                  credit_card_number: contact_hash['credit_card_number'],
+                                  credit_card_franchise: contact_hash['credit_card_franchise'])
       contact.update(contact_hash)
     end
   end
