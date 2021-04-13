2ba5c8f12352f524c1648df292fee4386663178a
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-13 15:19:41 -0500

    Credit card validation and match with franchise name were implemented

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,16 +1,20 @@
 class Contact < ApplicationRecord
   validates :name, :email, :address, :birthday, :credit_card_number, :credit_card_franchise, presence: true
-  
+
   validates :name, format: { with: /\A[a-zA-Z\s-]+\z/, message: 'cannot have special characters, you can use only "-" ' }
   validates :telephone, presence: true,
             format: { with: /\A(\(\+\d{2}\)[\s]\d{3}[\s]\d{3}[\s]\d{2}[\s]\d{2})|(\(\+\d{2}\)[\s]\d{3}[-]\d{3}[-]\d{2}[-]\d{2})\z/,
             message: "(+00) 000 000 00 00 and (+00) 000-000-00-00 are the only telephone formats permitted" }
 
+  validates :credit_card_number, credit_card_number: true
   validate :birthday_format
 
   def birthday_format
     Date.iso8601(birthday)
   end
+
+  # This way work to detect credit card brand
+  # credit_card_franchise: CreditCardValidations::Detector.new(contact_hash['credit_card_number']).brand
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
@@ -19,7 +23,7 @@ class Contact < ApplicationRecord
                                   email: contact_hash['email'], address: contact_hash['address'],
                                   birthday: contact_hash['birthday'],
                                   credit_card_number: contact_hash['credit_card_number'],
-                                  credit_card_franchise: contact_hash['credit_card_franchise'])
+                                  credit_card_franchise: contact_hash['credit_card_number'].credit_card_brand_name)
       contact.update(contact_hash)
     end
   end
