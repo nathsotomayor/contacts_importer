b371336b4229328ad8ff42e4730c90b0d8e4ae89
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-14 08:13:11 -0500

    Create many to many relationship between users and contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ApplicationRecord
+  has_many :users_contacts
+  has_many :users, through: :users_contacts
+
   validates :name, :email, :address, :birthday, :credit_card_number, :credit_card_franchise, presence: true
 
   validates :name, format: { with: /\A[a-zA-Z\s-]+\z/, message: 'cannot have special characters, you can use only "-" ' }
@@ -6,15 +9,13 @@ class Contact < ApplicationRecord
             format: { with: /\A(\(\+\d{2}\)[\s]\d{3}[\s]\d{3}[\s]\d{2}[\s]\d{2})|(\(\+\d{2}\)[\s]\d{3}[-]\d{3}[-]\d{2}[-]\d{2})\z/,
             message: "(+00) 000 000 00 00 and (+00) 000-000-00-00 are the only telephone formats permitted" }
 
-  validates :credit_card_number, credit_card_number: true
+  #validates :credit_card_number, credit_card_number: true
+  #attr_encrypted :credit_card_number, key: 'This is a key that is 256 bits!!'
   validate :birthday_format
 
   def birthday_format
     Date.iso8601(birthday)
   end
-
-  # This way work to detect credit card brand
-  # credit_card_franchise: CreditCardValidations::Detector.new(contact_hash['credit_card_number']).brand
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
@@ -22,7 +23,7 @@ class Contact < ApplicationRecord
       contact = find_or_create_by!(name: contact_hash['name'], telephone: contact_hash['telephone'],
                                   email: contact_hash['email'], address: contact_hash['address'],
                                   birthday: contact_hash['birthday'],
-                                  credit_card_number: contact_hash['credit_card_number'],
+                                  credit_card_number: contact_hash['credit_card_number'].last(4),
                                   credit_card_franchise: contact_hash['credit_card_number'].credit_card_brand_name)
       contact.update(contact_hash)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :users_contacts
+  has_many :contacts, through: :users_contacts
+
   validates :password, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
 
