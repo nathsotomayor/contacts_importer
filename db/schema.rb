4a9ef4479ef8a3303caec1dbfff10b33a9b4e5ae
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-14 15:39:42 -0500

    Change field from contacts by another to save last digits of credit card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_152019) do
+ActiveRecord::Schema.define(version: 2021_04_14_203018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_152019) do
     t.string "credit_card_number", default: "", null: false
     t.string "credit_card_franchise", default: "", null: false
     t.integer "status"
-    t.string "error", default: ""
+    t.string "credit_card_last_digits", default: ""
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
