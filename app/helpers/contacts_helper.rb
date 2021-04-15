3ad5a00ccd04450176d1b159f54082121da3213c
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-04-15 12:06:38 -0500

    Update birthday helper

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,5 +1,11 @@
 module ContactsHelper
   def custom_format_date(date)
-    Date.iso8601(date).strftime('%Y %B %e')
+    formatted_date = ''
+    begin
+      formatted_date = Date.iso8601(date).strftime('%Y %B %e')
+    rescue StandardError
+      formatted_date = date
+    end
+    formatted_date
   end
 end
